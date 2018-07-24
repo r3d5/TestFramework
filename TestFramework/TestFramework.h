93e2cb282e838c476cf508f46b19eb5a54ca44ae
Maksym Tsymbal <maksym.tsymbal@gmail.com>
2018-07-24 10:14:04 +0200

    Initial Commit

---
+++ TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Maksym Tsymbal on 24/07/2018.
+//  Copyright © 2018 Maksym Tsymbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
